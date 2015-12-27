d9157cf3597e5d8260839da72f4ce8b1dbfbd22e
Radu Nicolae <ranicolae@gmail.com>
2015-12-26 20:24:07 -0500

    output of ffmpeg less verbose

--- capture_rtp.sh
+++ capture_rtp.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ffmpeg -y -framerate 15 -f avfoundation -i "1:none" -vcodec libx264 -preset ultrafast -vf scale=640:-1 -f mulaw -f rtp rtp://127.0.0.1:1234
+ffmpeg -y -framerate 15 -pix_fmt uyvy422 -f avfoundation -i "1:none" -vcodec libx264 -preset ultrafast -vf scale=640:-1 -f mulaw -f rtp rtp://127.0.0.1:1234
