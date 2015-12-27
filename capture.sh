e39b5ffe3c2c24432c0fcc4c82c131c06e246eb1
Radu Nicolae <ranicolae@gmail.com>
2015-12-26 20:24:36 -0500

    removed file based stream

--- capture.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-ffmpeg -y -framerate 10 -async 1 -pix_fmt uyvy422 -s 1280x800 -threads 2 -f avfoundation -i "1:none" -vcodec libx264 -tune zerolatency -preset ultrafast -crf 0 -vf scale=640:-1 stream/out.h264
