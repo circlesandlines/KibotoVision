7f65d9d708ca154b323f1d0c2ca33614b1ea05bd
Radu Nicolae <ranicolae@gmail.com>
2015-12-31 01:12:34 -0500

    added the SURF feature detector in the pil sample bot

--- pil_sample_bot.py
+++ pil_sample_bot.py
@@ -1,9 +1,29 @@
-
+import cv2
 import brain
 
 class MyBrain9000(brain.Brain):
 	def think(self, imgdata):
-		self._debug(imgdata)
+                # process the image
+
+                # analyze the image to gain some knowledge about the
+                # player's situation
+                gray = cv2.cvtColor(imgdata, cv2.COLOR_BGR2GRAY)
+                surf = cv2.xfeatures2d.SURF_create(20000)
+
+                kp, des = surf.detectAndCompute(gray,None)
+
+                # draw the points onto a new image
+                processed = cv2.drawKeypoints(gray,kp,None,(255,0,0),4)
+
+                # perform action (courtesy of pyautogui)
+                # assuming some knowledge was gained form the analysis above
+                # uncomment below to move a player forward and shoot in an FPS game
+                #self.pag.press('up')
+                #self.pag.click()
+
+                # print the processed frame
+                self._debug(processed)
+
 
 if __name__=="__main__":
 	b = MyBrain9000(fps=30)
