7be6fdf3c8d344e1283dec5a588a255d15de2b7a
Radu Nicolae <ranicolae@gmail.com>
2016-05-02 19:30:02 -0400

    python 3 friendly print statements in the sample bot

--- samples/pil_sample_bot.py
+++ samples/pil_sample_bot.py
@@ -8,21 +8,21 @@ class MyBrain9000(brain.Brain):
 		# make the image smaller so that processing is faster
 		# change to quarter screen size
 		derezzed = cv2.resize(imgdata, None, fx=0.25, fy = 0.25)
-		print "screenshot derezzed"
+		print("screenshot derezzed")
 
 		# analyze the image to gain some knowledge about the
 		# player's situation
 		gray = cv2.cvtColor(derezzed, cv2.COLOR_BGR2GRAY)
-		print "enable grayscale"
+		print("enable grayscale")
 		surf = cv2.xfeatures2d.SURF_create(10000)
-		print "prime SURF algorithm"
+		print("prime SURF algorithm")
 
 		kp, des = surf.detectAndCompute(gray,None)
-		print "execute SURF"
+		print("execute SURF")
 
 		# draw the points onto a new image
 		processed = cv2.drawKeypoints(gray,kp,None,(255,0,0),4)
-		print "draw detected points to window"
+		print("draw detected points to window")
 
 		# perform action (courtesy of pyautogui)
 		# assuming some knowledge was gained form the analysis above
